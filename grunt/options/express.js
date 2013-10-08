19cde05b1a2595927c8e614f9560ea2f8cf82298
vi <noreply@example.org>
2013-10-08 19:27:01 +0800

    grunt: working server configuration

--- grunt/options/express.js
+++ grunt/options/express.js
@@ -1,24 +1,24 @@
 module.exports = function (grunt) {
   return {
     production: {
-      server: 'server.js',
       options: {
+        server: 'server.js',
         port: 9000,
         serverreload: false,
         open: true
       }
     },
     staging: {
-      server: 'server.js',
       options: {
+        server: 'server.js',
         port: 9001,
         serverreload: false,
         open: false
       }
     },
     development: {
-      server: 'server.js',
       options: {
+        server: 'server.js',
         port: 9002,
         serverreload: true,
         open: true
