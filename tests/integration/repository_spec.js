7d5900833f2b8ed448db5fdcd089dab39c6bda10
vi <noreply@example.org>
2013-10-09 13:05:42 +0800

    repository spec -> new frisby helper interface

--- tests/integration/repository_spec.js
+++ tests/integration/repository_spec.js
@@ -1,10 +1,10 @@
 var frisby               = require('frisby');
-var mkFrisbyJSON200      = require('../helpers/frisby.js').mkFrisbyJSON200;
+var mkFrisbyGETJSON200      = require('../helpers/frisby.js').mkFrisbyGETJSON200;
 var isGithubOrganization = require('../helpers/typecheck.js').isGithubOrganization;
 var isGithubRepository   = require('../helpers/typecheck.js').isGithubRepository;
 var allHaveProperty      = require('../helpers/typecheck.js').allHaveProperty;
 
-mkFrisbyJSON200(
+mkFrisbyGETJSON200(
   'List repos by organisation',
   'repositories/?github_organisation=someid',
   { organizations: Array,
