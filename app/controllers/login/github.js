3d78072aabc63b49573d5c8a62681427ca95ce0c
vi <noreply@example.org>
2013-10-12 06:10:45 +0800

    factor gh auth: fat model/skinny controller

--- app/controllers/login/github.js
+++ app/controllers/login/github.js
@@ -1,5 +1,5 @@
-define(['request', 'app/components/utils/uri', 'app/models/login', 'config/config'], 
-function (request, uri, login, config) {
+define(['app/components/utils/uri', 'app/models/login', 'config/config'], 
+function (uri, login, config) {
   var endpoints = config.endpoints;
   var keys = config.keys;
 
@@ -9,16 +9,6 @@ function (request, uri, login, config) {
       session: state
     });
   };
-
-  function mkLogin(code, session) { 
-    getAccessToken(code, function(body) {
-      var parameters = uri.fromParameterString(body);
-      login.create({
-        session: session,
-        access_token: parameters.access_token
-      });
-    });
-  }
 
   function authorize(req, res) {
     var later = req.query.redirect_uri
@@ -34,22 +24,11 @@ function (request, uri, login, config) {
   function redirect(req, res) {
     var code    = req.query.code;
     var session = req.query.session;
-    mkLogin(code, session);
+    login.make(code, session);
     var after = req.query.redirect_uri;
     res.redirect(after);
   };
   
-  function getAccessToken(code, callback) {
-    var url = uri.toURL(endpoints.ghAccessToken, {
-      client_id: keys.ghClientId,
-      client_secret: keys.ghClientSecret,
-      code: code
-    });
-    request.post(url, {}, function(err,resp,body) {
-      callback(body);
-    });
-  };
-
   return {
     authorize: authorize,
     redirect: redirect
--- app/models/login/github.js
+++ app/models/login/github.js
@@ -1,8 +1,34 @@
-define(['mongoose'], function(m) {
+define(['mongoose','request','app/components/utils/uri', 'config/config'], function(m,request,uri,config) {
+  var endpoints = config.endpoints;
+  var keys = config.keys;
+
   var schema = new m.Schema({
     session: String,
     access_token: String
   });
+  
+  function getAccessToken(code, callback) {
+    var url = uri.toURL(endpoints.ghAccessToken, {
+      client_id: keys.ghClientId,
+      client_secret: keys.ghClientSecret,
+      code: code
+    });
+    request.post(url, {}, function(err,resp,body) {
+      callback(body);
+    });
+  };
+  
+  schema.static('make', function(code, session) {
+    var self = this;
+    getAccessToken(code, function(body) {
+      var parameters = uri.fromParameterString(body);
+      new self({
+        session: session,
+        access_token: parameters.access_token
+      }).save();
+    });
+  });
+
 
   return {
     schema: schema
