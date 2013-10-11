1746886a4cbf9212ea54406afa359ea39d8c606b
vi <noreply@example.org>
2013-10-12 04:47:03 +0800

    github controller: single reference to each key and endpoint

--- app/controllers/login/github.js
+++ app/controllers/login/github.js
@@ -1,6 +1,18 @@
 define(['request', 'app/components/utils/uri', 'app/models/login'], function (request, uri, login) {
+
+  var keys = {
+    ghClientId: 'a110297b7d6a6fab5dac',
+    ghClientSecret: '1f452780e9f37f5c49a3406e60917e0c1032fae7' // Abuse wisely. ;-)
+  }
+
+  var endpoints = {
+    dwRedirect: "http://localhost:9000/login/github/redirect",
+    ghAuthorize: "https://github.com/login/oauth/authorize",
+    ghAccessToken: "https://github.com/login/oauth/access_token"
+  };
+
   function redirectURL(where,state) {
-    return uri.toURL("http://localhost:9000/login/github/redirect", {
+    return uri.toURL(endpoints.dwRedirect, {
       redirect_uri: where,
       session: state
     });
@@ -9,14 +21,21 @@ define(['request', 'app/components/utils/uri', 'app/models/login'], function (re
   function authorize(req, res) {
     var later = req.query.redirect_uri
     var state = req.query.session
-    var url = uri.toURL("https://github.com/login/oauth/authorize", {
+    var url = uri.toURL(endpoints.ghAuthorize, {
       redirect_uri: redirectURL(later,state),
-      client_id: 'a110297b7d6a6fab5dac',
+      client_id: keys.ghClientId,
       scope: 'repo'
     });
     res.redirect(url);
   }; 
-
+  
+  function redirect(req, res) {
+    var code    = req.query.code;
+    var session = req.query.session;
+    getAccessToken(code, saveWithSession(session));
+    var after = req.query.redirect_uri;
+    res.redirect(after);
+  };
 
   function saveWithSession(session) {
     return function(str) {
@@ -28,18 +47,10 @@ define(['request', 'app/components/utils/uri', 'app/models/login'], function (re
     } 
   }
 
-  function redirect(req, res) {
-    var code    = req.query.code;
-    var session = req.query.session;
-    getAccessToken(code, saveWithSession(session));
-    var after = req.query.redirect_uri;
-    res.redirect(after);
-  };
-
   function getAccessToken(code, callback) {
-    var url = uri.toURL('https://github.com/login/oauth/access_token', {
-      client_id: 'a110297b7d6a6fab5dac',
-      client_secret: '1f452780e9f37f5c49a3406e60917e0c1032fae7', // Abuse wisely. ;-)
+    var url = uri.toURL(endpoints.ghAccessToken, {
+      client_id: keys.ghClientId,
+      client_secret: keys.ghClientSecret,
       code: code
     });
     request.post(url, {}, function(err,resp,body) {
