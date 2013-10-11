c8866f9d2f7df3b18b83a3cc10443c711c2182c8
vi <noreply@example.org>
2013-10-11 12:52:58 +0800

    Revert "[REVERTME] ..."
    This reverts commit 92e4facb0cdb2850a795f5a2b38eeff3d0db89b6.

--- grunt/options/exec.js
+++ grunt/options/exec.js
@@ -2,9 +2,6 @@ module.exports = function(grunt) {
   return {
     frisby: {
       command: 'node ./node_modules/jasmine-node/bin/jasmine-node ./tests/integration'
-    },
-    express: {
-      command: 'PORT=9002 node ./server.js'
     }
   };
 };
