2855b8a6cbad6ee7ff2c75ae4f805ff9be8928ae
vi <noreply@example.org>
2013-10-11 14:17:57 +0800

    You broke everything with that frisby.js merge conflict.
    This reverts commit 772f44bd4000c2a66dc7dcf17f14be5d3dc4ad21, reversing
    changes made to eb1f14c4c9b6b6f0a45b5f5737652a0904e8aaf0.

--- tests/helpers/frisby.js
+++ tests/helpers/frisby.js
@@ -1,39 +1,82 @@
 var frisby   = require('frisby');
 var fromPath = require('./endpoint.js').endpoint;
+var _        = require('underscore');
 
-function mkFrisby(desc, path, method, body, response, type, output, sub_output) {
-  var endpoint = fromPath(path);
-  return frisby.create(desc)[method](endpoint, body)
-               .expectStatus(response)
-               .expectHeaderContains('content-type', type)
-               .expectJSONTypes('0', output)
-               .expectJSONTypes('0', sub_output);
+var parameters = {
+//method:     function(f,o) { return f[o]; },
+//endpoint:   function(g,o) { return function(body) { return g(fromPath(o), body); };},
+//body:       function(h,o) { return h(o); },
+  response:   function(f,o) { return f.expectStatus(o); },
+  type:       function(f,o) { return f.expectHeaderContains('content-type', o); },
+  output:     function(f,o) { return f.expectJSONTypes('0',o); },
+  sub_output: function(f,o) { return f.expectJSONTypes('0',o); }
+};
+
+var frisbyDefaults = {
+  body:       {},
+  output:     {},
+  sub_output: {}
+};
+
+function mkFrisbyRequest(desc, options) {
+  var frisbee = frisby.create(desc);
+  var m = options.method,
+      e = fromPath(options.endpoint),
+      b = options.body;
+  return frisbee[m](e,b);
 }
 
-function mkFrisbyGET(desc, path, response, type, output, sub_output) {
-  return mkFrisby(desc, path, 'get', {}, response, type, output, sub_output);
+function mkFrisby(desc, mkOptions, mkDefaults) {
+  var defaults = _.extend(frisbyDefaults, mkDefaults);
+  var options  = _.defaults(mkOptions, defaults);
+  var frisbee  = mkFrisbyRequest(desc, options);
+  _(parameters).each(function(fn, par){
+    var option = options[par];
+    if (option) { frisbee = fn(frisbee, option); }
+  }); 
+  return frisbee;
 }
 
-function mkFrisbyGETJSON200(desc, path, output, sub_output) {
-  return mkFrisbyGET(desc,path,200,'application/json',output,sub_output);
+function mkFrisbyGET(description, options) {
+  return mkFrisby(description, options, { 
+    method: 'get'
+  });
 }
 
-function mkFrisbyPOST(desc, path, body, response, type, output, sub_output) {
-  return mkFrisby(desc, path, 'post', body, response, type, output, sub_output);
+function mkFrisbyGETJSON200(description,options) {
+  return mkFrisby(description, options, {
+    method: 'get',
+    response: 200,
+    type: 'application/json'
+  });
 }
 
-function mkFrisbyPUT(desc, path, body, response, type, output, sub_output) {
-  return mkFrisby(desc, path, 'put', body, response, type, output, sub_output);
+function mkFrisbyPOST(description,options) {
+  return mkFrisby(description, options, {
+    method: 'post'
+  });
 }
 
-function mkFrisbyDELETE(desc, path, response, type, output, sub_output) {
-  return mkFrisby(desc, path, 'delete', {}, response, type, output, sub_output);
+
+function mkFrisbyPUT(description,options) {
+  return mkFrisby(description, options, {
+    method: 'put'
+  });
 }
 
-function mkFrisbyGET302(desc, path) {
-  return mkFrisby(desc, path, 'get', {}, 302, 'text/html', {}, {})
+function mkFrisbyDELETE(description,options) {
+  return mkFrisby(description, options, {
+    method: 'delete'
+  });
 }
 
+function mkFrisbyGET302(description,options) {
+  return mkFrisby(description, options, { 
+    method: 'get',
+    response: 302,
+    type: 'text/html'
+  });
+}
 
 module.exports = {
   mkFrisby:            mkFrisby,
@@ -43,4 +86,4 @@ module.exports = {
   mkFrisbyDELETE:      mkFrisbyDELETE,
   mkFrisbyGETJSON200:  mkFrisbyGETJSON200,
   mkFrisbyGET302: mkFrisbyGET302
-}
+};
