bd35a23cdca4206f7c0aa785f26e6c2bd960b93d
Alyssa <alyssaquek@gmail.com>
2014-08-20 13:16:04 +0800

    Update tests

--- tests/api/coordinates_spec.js
+++ tests/api/coordinates_spec.js
@@ -1,17 +1,31 @@
+var path = require('path');
 var frisby = require('frisby');
 var utils = require('../helpers/utils');
 
+var owner = 'cofounders';
+var repo = 'drywall-web';
+var coordinatesUrl = utils.localhost(
+  path.join(owner, repo, 'coordinates')
+);
+
 frisby.create('Test getting coordinates')
-  .get(utils.endpoint('/cofounders/drywall/coordinates'))
+  .get(coordinatesUrl)
   .expectStatus(200)
+  .expectJSONTypes('?', {
+    number: Number,
+    x: Number,
+    y: Number,
+    owner: owner,
+    repo: repo
+  })
 .toss();
 
 frisby.create('Test posting missing coordinates')
-  .post(utils.endpoint('/cofounders/drywall/coordinates'))
+  .post(coordinatesUrl)
   .expectStatus(400)
 .toss();
 
 frisby.create('Test updating missing number')
-  .post(utils.endpoint('/cofounders/drywall/coordinates?x=100&y=20'))
+  .post(coordinatesUrl + '?x=100&y=20')
   .expectStatus(400)
 .toss();
--- tests/helpers/utils.js
+++ tests/helpers/utils.js
@@ -1,9 +1,10 @@
-function endpoint(path) {
+function localhost(path) {
   var base = 'http://localhost';
   var port = require('../../app/config').port;
+  path = path[0] === '/' ? path : '/' + path;
   return [base, ':', port, path || ''].join('');
 }
 
 module.exports = {
-  endpoint: endpoint
+  localhost: localhost
 };
