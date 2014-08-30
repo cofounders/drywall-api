b4b120def4cb54dee52d39778bc9c92711be71e0
Alyssa <alyssaquek@gmail.com>
2014-08-30 15:12:07 +0800

    Added timestamp to transactions

--- app/models/transactions.js
+++ app/models/transactions.js
@@ -1,7 +1,11 @@
 var mongoose = require('mongoose');
 
 var TransactionSchema = new mongoose.Schema({
-  data: mongoose.Schema.Types.Mixed
+  data: mongoose.Schema.Types.Mixed,
+  timestamp: {
+    type: Date,
+    default: Date.now
+  },
 });
 
 module.exports = mongoose.model('Transaction', TransactionSchema);
