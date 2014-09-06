2d14fbc38e0adff51fa1ee83fe103138b6086d21
Alyssa <alyssaquek@gmail.com>
2014-09-06 20:09:20 +0800

    IPN listener updates account status if user cancelled directly from PayPal account

--- app/routes/ipnListener.js
+++ app/routes/ipnListener.js
@@ -5,13 +5,30 @@ var prequest = require('../modules/prequest');
 var config = require('../config');
 var TransactionsModel = require('../models/transactions');
 var ppConfig = config.paypal[config.paypal.mode];
+var AccountsModel = require('../models/accounts');
+var consts = require('../modules/consts');
+
+function updateAccountStatus(data) {
+  AccountsModel.findOne({
+    paymentId: data.recurring_payment_id,
+    status: consts.active
+  }, function (err, account) {
+    if (!err && account && account.status !== data.profile_status) {
+      account.status = data.profile_status;
+      account.save();
+      console.log('WARNING! ', account.owner,
+        account.user + ' may have cancelled via PayPal');
+    }
+  });
+}
 
 function saveTransaction(data) {
   var transaction = new TransactionsModel({data: data});
 
   transaction.save(function (err) {
     if (!err) {
-      console.log(data.recurring_payment_id,
+      console.log('Paypal IPN saved: ',
+        data.recurring_payment_id,
         data.product_name,
         data.profile_status
       );
@@ -23,7 +40,6 @@ function saveTransaction(data) {
 
 function ipnListener(req, res) {
   var data = req.body;
-  console.log('Paypal IPN: ', data);
   res.send();
 
   if (req.headers.host.indexOf('drywall') !== -1) {
@@ -40,6 +56,9 @@ function ipnListener(req, res) {
     console.log('Paypal IPN response: ' + body);
     if (body === 'VERIFIED') {
       saveTransaction(data);
+      if (data.profile_status === consts.cancelled) {
+        updateAccountStatus(data);
+      }
     }
   }).catch(function (err) {
     console.error('Error verifying paypal IPN', ppConfig.paymentUrl, err);
